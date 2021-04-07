d12a2ecafccde94455012d4c11d521d067090e88
Johnny Nguyen <cuongdn2001@gmail.com>
2021-04-07 15:29:50 +1000

    Customer LogIn Functionality

--- eRestaurant/src/main/java/com/softwarestudiogroup1/uts/eRestaurant/controllers/customer/CustomerController.java
+++ eRestaurant/src/main/java/com/softwarestudiogroup1/uts/eRestaurant/controllers/customer/CustomerController.java
@@ -3,10 +3,12 @@ package com.softwarestudiogroup1.uts.eRestaurant.controllers.customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import com.softwarestudiogroup1.uts.eRestaurant.models.CustomerRepository;
 import com.softwarestudiogroup1.uts.eRestaurant.models.entities.Customer;
@@ -32,6 +34,23 @@ public class CustomerController {
 
         return "customers/allcustomers";
         
+    }
+
+    @GetMapping("/booking")
+    public String getBookingPage(@ModelAttribute("customerID") int customerID, Model model) {
+      
+        Optional<Customer> currentCus = customerRepository.findById(customerID);
+
+        if (currentCus.isPresent()) {
+             Customer customer = currentCus.get();
+             model.addAttribute("customerName", customer.getFirstName());
+        }
+
+        else {
+            return "redirect:/";
+        }
+
+        return "customers/bookingPage";
     }
 
 }
