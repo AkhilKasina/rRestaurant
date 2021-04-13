9a57b0a5e2329c9db0979ceb1a9d6cb4a44e4c15
Johnny Nguyen <cuongdn2001@gmail.com>
2021-04-13 13:15:01 +1000

    Update with h2 database

--- eRestaurant/src/main/resources/database/data.sql
+++ eRestaurant/src/main/resources/database/data.sql
@@ -1,4 +1,4 @@
-INSERT IGNORE INTO customers (id, username, password, first_name, last_name, telephone, address)
+INSERT INTO customers (id, username, password, first_name, last_name, telephone, address)
 VALUES(1, 'johnny', '123', 'Johnny', 'Nguyen', '012345634', '123 Add Street');
 
-INSERT IGNORE INTO bookings VALUES(1, '2021-01-02 20:30:00', '3E', 1);
+INSERT INTO bookings VALUES(1, '2021-01-02 20:30:00', '3E', 1);
