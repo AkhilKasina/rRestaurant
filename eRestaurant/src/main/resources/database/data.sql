15f9bbd9239f5651f24c404161fa5904783f5665
Michael-Dowsett <62222887+Michael-Dowsett@users.noreply.github.com>
2021-04-14 09:45:18 +1000

    sql menu items

--- eRestaurant/src/main/resources/database/data.sql
+++ eRestaurant/src/main/resources/database/data.sql
@@ -2,3 +2,11 @@ INSERT INTO customers (id, username, password, first_name, last_name, telephone,
 VALUES(1, 'johnny', '123', 'Johnny', 'Nguyen', '012345634', '123 Add Street');
 
 INSERT INTO bookings VALUES(1, '2021-01-02 20:30:00', '3E', 1);
+
+INSERT INTO items (id, name, image_data, menu_type)
+VALUES(001, 'Pizza', NULL, 'Lunch'),
+(002, 'Pasta', NULL, 'Lunch'),
+(003, 'Burger', NULL, 'Lunch'),
+(004, 'Soup', NULL, 'Dinner'),
+(005, 'Nachos', NULL, 'Dinner'),
+(006, 'Steak', NULL, 'Dinner');
