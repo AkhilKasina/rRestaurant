386641296a6c9ed6cf68789b2e22d29e29ffc14a
Alexander Rahme <alexrahme@Alexs-MacBook-Pro.local>
2021-04-21 10:31:23 +1000

    menu items backend

--- eRestaurant/target/classes/database/data.sql
+++ eRestaurant/target/classes/database/data.sql
@@ -5,3 +5,5 @@ INSERT INTO bookings VALUES(1, '2021-01-02 20:30:00', '3E', 1);
 
 
 INSERT INTO managers VALUES(1, 'M100001@manager.com', 'M100001', '123', 'Software', 'Engineering', '044469696');
+
+INSERT INTO items VALUES(1, 'chicken tendies', 10.00, 'lunch');
