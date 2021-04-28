4dc93f7580d1d08482449ee85a9a719a37b53111
Kai Sor <kai.p.sor@student.uts.edu.au>
2021-04-28 22:04:02 +1000

    menu with sql done

--- eRestaurant/src/main/java/com/softwarestudiogroup1/uts/eRestaurant/controllers/customer/OrderController.java
+++ eRestaurant/src/main/java/com/softwarestudiogroup1/uts/eRestaurant/controllers/customer/OrderController.java
@@ -1,6 +1,7 @@
 package com.softwarestudiogroup1.uts.eRestaurant.controllers.customer;
 
 import com.softwarestudiogroup1.uts.eRestaurant.models.entities.Customer;
+import com.softwarestudiogroup1.uts.eRestaurant.models.entities.Item;
 import com.softwarestudiogroup1.uts.eRestaurant.models.entities.Patron;
 
 import org.springframework.stereotype.Controller;
@@ -9,25 +10,39 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.text.View;
 
 import com.softwarestudiogroup1.uts.eRestaurant.ViewManager;
 import com.softwarestudiogroup1.uts.eRestaurant.models.CustomerRepository;
+import com.softwarestudiogroup1.uts.eRestaurant.models.ItemRepository;
 
 import org.springframework.ui.Model;
 
 @Controller
 public class OrderController {
 
-    @GetMapping("/orderPage")
-    public String orderPage(Model model) {
-        return ViewManager.CUS_ORDER;
+    private final ItemRepository itemRepository;
+
+    public OrderController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
     }
 
-    @GetMapping("/orderConfirmation")
-    public String orderConfirmation(Model model) {
-        return ViewManager.CUS_CONFIRM;
+    @GetMapping("/orderPage")
+    public String orderPage(Model model) {
+        List<Item> itemLists = itemRepository.findAll();
+        List<Item> lunchList = new ArrayList<>();
+
+        //only puts lunch items into lunchList
+        for(Item item : itemLists){
+            if(item.getMenuType().equals("lunch")){
+                lunchList.add(item);
+            }
+        }
+
+        model.addAttribute("Items", lunchList);
+        return "customers/orderPage";
     }
 }
